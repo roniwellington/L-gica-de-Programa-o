f48bfe90cdcc174f907b94d53b3351b6a0e259a7
roniwellington <roniwellington@gmail.com>
2023-10-26 00:50:41 -0300

    estudo de lógica de programação

--- js-curso-2-aula_-/app.js
+++ js-curso-2-aula_-/app.js
@@ -1,9 +1,12 @@
+let listaDeNumerosSorteados = [];
+let numeroLimite = 10;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2})
 };
 
 function exibirMensagemInicial() {
@@ -12,7 +15,6 @@ function exibirMensagemInicial() {
 }
 
 exibirMensagemInicial();
-
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
@@ -35,7 +37,20 @@ function verificarChute() {
 };
 
 function gerarNumeroAleatorio() {
-    return parseInt(Math.random() * 10 + 1);
+    let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
+    let quantidadeDeElementosNaLista = listaDeNumerosSorteados.length;
+
+    if(quantidadeDeElementosNaLista == numeroLimite){
+        listaDeNumerosSorteados = [];
+    }
+
+    if(listaDeNumerosSorteados.includes(numeroEscolhido)) {
+        return gerarNumeroAleatorio();
+    } else {
+        listaDeNumerosSorteados.push(numeroEscolhido);
+        console.log(listaDeNumerosSorteados);
+        return numeroEscolhido;
+    }
 };
 
 function limparCampo() {
@@ -44,11 +59,11 @@ function limparCampo() {
 }
 
 function reiniciarJogo() {
-    numeroSecreto = gerarNumeroAleatorio;
+    numeroSecreto = gerarNumeroAleatorio();
     limparCampo();
     tentativas = 1;
     exibirMensagemInicial();
-    document.getElementById('reiniciar').setAttribute('disabled',true)
+    document.getElementById('reiniciar').setAttribute('disabled', true)
 }
 
 console.log(numeroSecreto);
---
+++ js-curso-2-aula_-/desafios/desafioQuatro.js
@@ -0,0 +1,20 @@
+//1.Crie uma lista vazia, com o nome listaGenerica.
+let listaGenerica = [];
+console.log(listaGenerica)
+
+//2.Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos: 'JavaScript','C','C++', 'Kotlin' e 'Python'.
+let linguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python']
+console.log(linguagensDeProgramacao)
+
+//3.Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.
+linguagensDeProgramacao.push('java','Ruby','GoLang')
+console.log(linguagensDeProgramacao)
+
+//4.Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+let listaComTresNome = ['Dina', 'Roni', 'Dâmaris']
+console.log(listaComTresNome[0])
+
+//5.Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
+console.log(listaComTresNome[1])
+//6.Crie uma lista com 3 nomes e exiba no console apenas o último elemento.
+console.log(listaComTresNome[2])
